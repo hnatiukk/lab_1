4fe32e17ebb658299184bd3cdf8ec5eecff5a332
Вова Гнатюк <vololdymyr.gn@gmail.com>
2022-09-24 20:07:01 +0300

    lab_1

---
+++ lab_1/lab_1/lab_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string name;
+    cout << "Введіть ваше ім'я: ";
+    cin >> name;
+    cout << "Вас звати " << name;
+}
